1fc9473b15709b4f40f9fa1f36ca8988f2ed780d
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2024-12-28 15:40:00 +0200

    colors for sign-up

--- app/src/main/java/com/example/zadumite_frontend/ui/theme/Color.kt
+++ app/src/main/java/com/example/zadumite_frontend/ui/theme/Color.kt
@@ -4,6 +4,8 @@ import androidx.compose.ui.res.colorResource
 import com.example.zadumite_frontend.R
 
 val White = Color(0xFFFFFFFF)
+val LightGray = Color(0xFFF0F0F0)
+val DarkGray = Color(0xD3D3D3)
 val Brown = Color(0xFF4D2D18)
 val Beige = Color(0xFFCBBA9E)
 val LightBrown = Color(0xFF8A6440)
