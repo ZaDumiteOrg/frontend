a9f5e3698a053b97cf6016b996953183003bef0f
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-08 18:52:07 +0200

    add data classes for user sign-up requests and responses

---
+++ app/src/main/java/com/example/zadumite_frontend/data/model/user/User.kt
@@ -0,0 +1,10 @@
+package com.example.zadumite_frontend.data.model.user
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val password: String,
+    val role: String,
+    val id: Int
+)
