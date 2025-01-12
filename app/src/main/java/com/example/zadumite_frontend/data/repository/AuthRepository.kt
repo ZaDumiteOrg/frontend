31ba8866f218bb69b2d91b2be389c94e148968fe
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-12 16:15:50 +0200

    added token management in login method

--- app/src/main/java/com/example/zadumite_frontend/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/zadumite_frontend/data/repository/AuthRepository.kt
@@ -1,10 +1,16 @@
 package com.example.zadumite_frontend.data.repository
 
-import ZaDumiteApiService
+import com.example.zadumite_frontend.network.ZaDumiteApiService
+import com.example.zadumite_frontend.data.model.token.JwtTokenManager
+import com.example.zadumite_frontend.data.model.token.TokenResponse
+import com.example.zadumite_frontend.data.model.user.LogInRequest
 import com.example.zadumite_frontend.data.model.user.SignUpRequest
 import com.example.zadumite_frontend.data.model.user.SignUpResponse
 
-class AuthRepository(private val apiService: ZaDumiteApiService) {
+class AuthRepository(
+    private val apiService: ZaDumiteApiService,
+    private val tokenManager: JwtTokenManager
+) {
     suspend fun signUp(user: SignUpRequest): SignUpResponse {
         try{
             val response = apiService.signUp(user)
@@ -18,6 +24,22 @@ class AuthRepository(private val apiService: ZaDumiteApiService) {
             println("Exception during sign up: ${e.message}")
             throw e
         }
+    }
 
+    suspend fun logIn(user: LogInRequest): TokenResponse {
+        try {
+            val response = apiService.logIn(user)
+            if (response.isSuccessful && response.body() != null) {
+                val body = response.body()!!
+                tokenManager.saveAccessJwt(body.accessToken)
+                tokenManager.saveRefreshJwt(body.refreshToken)
+                return body
+            } else {
+                throw Exception("Login failed: ${response.code()} - ${response.message()} - ${response.errorBody()?.string()}")
+            }
+        } catch (e: Exception) {
+            println("Exception during log in: ${e.message}")
+            throw e
+        }
     }
 }
