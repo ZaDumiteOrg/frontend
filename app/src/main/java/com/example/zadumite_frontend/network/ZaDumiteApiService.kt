a500bf51b4c0fbb4c1805c5872125a9be3dc3df2
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-13 22:33:18 +0200

    Endpoint for login api requests

--- app/src/main/java/com/example/zadumite_frontend/network/ZaDumiteApiService.kt
+++ app/src/main/java/com/example/zadumite_frontend/network/ZaDumiteApiService.kt
@@ -1,19 +1,17 @@
+package com.example.zadumite_frontend.network
+
+import com.example.zadumite_frontend.data.model.token.TokenResponse
+import com.example.zadumite_frontend.data.model.user.LogInRequest
 import com.example.zadumite_frontend.data.model.user.SignUpRequest
 import com.example.zadumite_frontend.data.model.user.SignUpResponse
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
-
-const val BASE_URL = "http://10.0.2.2:3000/"
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(GsonConverterFactory.create())
-    .baseUrl(BASE_URL)
-    .build()
 
 interface ZaDumiteApiService {
     @POST("auth/register")
     suspend fun signUp(@Body request: SignUpRequest): Response<SignUpResponse>
+
+    @POST("auth/login")
+    suspend fun logIn(@Body request: LogInRequest): Response<TokenResponse>
 }
