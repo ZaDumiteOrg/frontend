dde0870f23e5d400901b2405151be2e4661b75fc
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-13 22:05:10 +0200

    Updated app module

--- app/src/main/java/com/example/zadumite_frontend/di/AppModule.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/AppModule.kt
@@ -1,11 +1,10 @@
 package com.example.zadumite_frontend.di
 
 val appModule = listOf(
+    networkModule,
     jwtTokenModule,
     interceptorModule,
     authenticatorModule,
-    networkModule,
     repositoryModule,
-    viewModelModule,
-    tokenManagerModule,
+    viewModelModule
 )
