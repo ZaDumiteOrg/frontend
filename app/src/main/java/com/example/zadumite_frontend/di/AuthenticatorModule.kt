2a2b13109bbe957429e8a82bac2b797cafb4a0f8
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-13 15:22:13 +0200

    Updated authenticatorModule to provide RefreshTokenService and RefreshTokenHandler

--- app/src/main/java/com/example/zadumite_frontend/di/AuthenticatorModule.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/AuthenticatorModule.kt
@@ -1,9 +1,25 @@
 package com.example.zadumite_frontend.di
 
+import com.example.zadumite_frontend.network.RefreshTokenService
 import com.example.zadumite_frontend.network.authenticator.AuthAuthenticator
+import com.example.zadumite_frontend.network.authenticator.RefreshTokenHandler
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
-
+import retrofit2.Retrofit
 
 val authenticatorModule = module {
-    single { AuthAuthenticator(get(), get()) }
+    single<RefreshTokenService> {
+        get<Retrofit>(named("RefreshRetrofit")).create(RefreshTokenService::class.java)
+    }
+
+    single {
+        RefreshTokenHandler(get())
+    }
+
+    single {
+        AuthAuthenticator(
+            tokenManager = get(),
+            refreshTokenHandler = get()
+        )
+    }
 }
