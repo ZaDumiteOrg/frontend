f783bde39857953e4d57f99a6ea884490ae1e193
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-13 15:11:09 +0200

    Remove refreshToken request from ZaDumiteApiService and created a seperate service to remove circular dependency

---
+++ app/src/main/java/com/example/zadumite_frontend/network/RefreshTokenService.kt
@@ -0,0 +1,13 @@
+package com.example.zadumite_frontend.network
+
+import com.example.zadumite_frontend.data.model.token.RefreshTokenRequest
+import com.example.zadumite_frontend.data.model.token.TokenResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface RefreshTokenService {
+    @POST("auth/refresh")
+    suspend fun refreshToken(@Body refreshTokenRequest: RefreshTokenRequest): retrofit2.Response<TokenResponse>
+}
+
+
