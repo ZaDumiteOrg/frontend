2b244dec64754957baee1cf38579d04d0ad9d016
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-13 15:02:51 +0200

    Provider function for creating authenticated OkHttp clients

---
+++ app/src/main/java/com/example/zadumite_frontend/di/providers/provideAccessOkHttpClient.kt
@@ -0,0 +1,24 @@
+package com.example.zadumite_frontend.di.providers
+
+import com.example.zadumite_frontend.network.authenticator.AuthAuthenticator
+import com.example.zadumite_frontend.network.interception.AccessTokenInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+
+fun provideAccessOkHttpClient(
+    accessTokenInterceptor: AccessTokenInterceptor,
+    authAuthenticator: AuthAuthenticator
+): OkHttpClient {
+    val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    return OkHttpClient.Builder()
+        .authenticator(authAuthenticator)
+        .addInterceptor(accessTokenInterceptor)
+        .addInterceptor(loggingInterceptor)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+}
