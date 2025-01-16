092492c944e641b087514022d8973cc239b8cac7
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-16 08:49:58 +0200

    Endpoint for fetching new word every week

--- app/src/main/java/com/example/zadumite_frontend/network/ZaDumiteApiService.kt
+++ app/src/main/java/com/example/zadumite_frontend/network/ZaDumiteApiService.kt
@@ -4,8 +4,10 @@ import com.example.zadumite_frontend.data.model.token.TokenResponse
 import com.example.zadumite_frontend.data.model.user.LogInRequest
 import com.example.zadumite_frontend.data.model.user.SignUpRequest
 import com.example.zadumite_frontend.data.model.user.SignUpResponse
+import com.example.zadumite_frontend.data.model.word.Word
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ZaDumiteApiService {
@@ -14,4 +16,7 @@ interface ZaDumiteApiService {
 
     @POST("auth/login")
     suspend fun logIn(@Body request: LogInRequest): Response<TokenResponse>
+
+    @GET("user/word-of-the-week")
+    suspend fun getWordOfTheWeek(): Word
 }
