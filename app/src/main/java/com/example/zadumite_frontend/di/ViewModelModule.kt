1dbb1b26938a6742e9c16879a10f79c94eae56f6
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-16 08:52:30 +0200

    Added WordViemodel to module for DI

--- app/src/main/java/com/example/zadumite_frontend/di/ViewModelModule.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/ViewModelModule.kt
@@ -1,8 +1,12 @@
 package com.example.zadumite_frontend.di
+import com.example.zadumite_frontend.ui.login.LogInViewModel
 import com.example.zadumite_frontend.ui.signup.SignUpViewModel
-import org.koin.core.module.dsl.viewModelOf
+import com.example.zadumite_frontend.ui.word.WordViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModelOf(::SignUpViewModel)
+    viewModel { SignUpViewModel(get()) }
+    viewModel { LogInViewModel(get()) }
+    viewModel { WordViewModel(get()) }
 }
