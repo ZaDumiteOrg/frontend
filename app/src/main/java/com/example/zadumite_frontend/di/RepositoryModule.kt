24714f7afe963842cb5bddd51a5f22c2c888b449
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-30 14:51:33 +0200

    added WordRepository to repository module for DI

--- app/src/main/java/com/example/zadumite_frontend/di/RepositoryModule.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.example.zadumite_frontend.di
 
 import com.example.zadumite_frontend.data.repository.AuthRepository
+import com.example.zadumite_frontend.data.repository.WordRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single { AuthRepository(get(), get()) }
+    single { WordRepository(get(), get()) }
 }
