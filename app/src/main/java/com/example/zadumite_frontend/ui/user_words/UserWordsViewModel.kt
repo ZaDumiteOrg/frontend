8f0bbe2c261d6237179a59b16844cf2442515a00
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-30 13:09:27 +0200

    updated UserWordsViewModel to use JwtTokenManager to fetch user id

--- app/src/main/java/com/example/zadumite_frontend/ui/user_words/UserWordsViewModel.kt
+++ app/src/main/java/com/example/zadumite_frontend/ui/user_words/UserWordsViewModel.kt
@@ -1,29 +1,38 @@
 package com.example.zadumite_frontend.ui.user_words
 
 import android.util.Log
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.zadumite_frontend.data.model.word.Word
 import com.example.zadumite_frontend.network.ZaDumiteApiService
-import androidx.compose.runtime.State
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.zadumite_frontend.data.model.token.JwtTokenManager
 import kotlinx.coroutines.launch
 
 
-class UserWordsViewModel(private val apiService: ZaDumiteApiService) : ViewModel() {
-    private val _userWords = mutableStateOf<List<Word>>(emptyList())
-    val userWords: State<List<Word>> = _userWords
+class UserWordsViewModel(
+    private val apiService: ZaDumiteApiService,
+    private val userManager: JwtTokenManager
+) : ViewModel() {
+    private val _userWords = MutableLiveData<List<Word>>()
+    val userWords: LiveData<List<Word>> get() = _userWords
 
-    private val _loading = mutableStateOf(false)
-    val loading: State<Boolean> = _loading
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean> get() = _loading
 
-    fun fetchUserWords(userId: Int) {
+    private val _userId = MutableLiveData<String?>()
+    val userId: LiveData<String?> get() = _userId
+
+    fun fetchUserWords() {
         viewModelScope.launch {
             _loading.value = true
             try {
-                Log.d("UserWordsViewModel", "Fetching words for user: $userId")
-                val words = apiService.getUserWords(userId)
-                _userWords.value = words
+                val userId = userManager.getUserId()
+                if(userId != null) {
+                    val words = apiService.getUserWords(userId)
+                    _userWords.value = words
+                }
             } catch (e: Exception) {
                 Log.e("UserWordsViewModel", "Error fetching user words: ${e.message}")
             } finally {
