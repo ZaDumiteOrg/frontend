47fb6ef6784428be90e16fbd18fb5d6a13bc84f4
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-30 13:10:07 +0200

    UserWordsViewModel module accepts two arguments

--- app/src/main/java/com/example/zadumite_frontend/di/ViewModelModule.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.example.zadumite_frontend.di
 import com.example.zadumite_frontend.session.SessionViewModel
+import com.example.zadumite_frontend.ui.add_word.AddWordViewModel
 import com.example.zadumite_frontend.ui.login.LogInViewModel
 import com.example.zadumite_frontend.ui.signup.SignUpViewModel
 import com.example.zadumite_frontend.ui.user_words.UserWordsViewModel
@@ -12,5 +13,6 @@ val viewModelModule = module {
     viewModel { LogInViewModel(get()) }
     viewModel { WordViewModel(get()) }
     single { SessionViewModel() }
-    viewModel { UserWordsViewModel(get()) }
+    viewModel { UserWordsViewModel(get(), get()) }
+    viewModel{ AddWordViewModel(get())}
 }
