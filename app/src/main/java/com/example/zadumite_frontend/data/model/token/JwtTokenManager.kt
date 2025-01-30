daa6d1666bc7f3d55446748b199ef5151375e63f
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-30 12:50:28 +0200

    added user id methods to interface JwtTokenManager

--- app/src/main/java/com/example/zadumite_frontend/data/model/token/JwtTokenManager.kt
+++ app/src/main/java/com/example/zadumite_frontend/data/model/token/JwtTokenManager.kt
@@ -1,6 +1,9 @@
 package com.example.zadumite_frontend.data.model.token
 
 interface JwtTokenManager {
+
+    suspend fun saveUserId(userId: Int)
+    suspend fun getUserId(): Int?
     suspend fun saveAccessJwt(accessToken: String)
     suspend fun saveRefreshJwt(refreshToken: String)
     suspend fun getAccessJwt(): String?
