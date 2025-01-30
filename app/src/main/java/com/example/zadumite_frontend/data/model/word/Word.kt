6b5e921a8dfffc801644b6dad3a52cc9f669ecf5
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-30 14:50:15 +0200

    word data class

--- app/src/main/java/com/example/zadumite_frontend/data/model/word/Word.kt
+++ app/src/main/java/com/example/zadumite_frontend/data/model/word/Word.kt
@@ -3,5 +3,6 @@ package com.example.zadumite_frontend.data.model.word
 data class Word (
     val word: String,
     val description: String,
-    val example: String
+    val example: String,
+    val synonym: String? = null
 )
