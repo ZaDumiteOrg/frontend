2cf421e17809ec6ea5fff3ba36545517a65a3f87
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-31 08:32:17 +0200

    updated interception to handle 401 return code

--- app/src/main/java/com/example/zadumite_frontend/network/interception/AccessTokenInterceptor.kt
+++ app/src/main/java/com/example/zadumite_frontend/network/interception/AccessTokenInterceptor.kt
@@ -1,9 +1,7 @@
 package com.example.zadumite_frontend.network.interception
 
 import com.example.zadumite_frontend.data.model.token.JwtTokenManager
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -16,9 +14,7 @@ class AccessTokenInterceptor(
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val token = runBlocking {
-            tokenManager.getAccessJwt()
-        }
+        val token = runBlocking { tokenManager.getAccessJwt() }
 
         val request = chain.request().newBuilder().apply {
             if (!token.isNullOrEmpty()) {
@@ -26,7 +22,24 @@ class AccessTokenInterceptor(
             }
         }.build()
 
-        return chain.proceed(request)
+        var response = chain.proceed(request)
+
+        if (response.code == 401) {
+            synchronized(this) {
+                val newToken = runBlocking { tokenManager.getRefreshJwt() }
+                if (!newToken.isNullOrEmpty() && newToken != token) {
+                    runBlocking { tokenManager.saveAccessJwt(newToken) }
+
+                    val newRequest = request.newBuilder()
+                        .header(HEADER_AUTHORIZATION, "$TOKEN_TYPE $newToken")
+                        .build()
+
+                    response.close()
+                    response = chain.proceed(newRequest)
+                }
+            }
+        }
+
+        return response
     }
 }
-
