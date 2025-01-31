39a922bb212689c82b8aab27f1e49d55754367bc
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-31 10:05:18 +0200

    changed order of word details to fit backend word entity

--- app/src/main/java/com/example/zadumite_frontend/domain/AddWordUseCase.kt
+++ app/src/main/java/com/example/zadumite_frontend/domain/AddWordUseCase.kt
@@ -6,11 +6,11 @@ import com.example.zadumite_frontend.data.repository.WordRepository
 class AddWordUseCase(private val wordRepository: WordRepository) {
     suspend operator fun invoke(
         word: String,
-        example: String,
         description: String,
+        example: String,
         synonym: String? = null
     ): Result<Word> {
-        val wordRequest = Word(word, example, description, synonym)
+        val wordRequest = Word(word, description, example, synonym)
         return wordRepository.addWord(wordRequest)
     }
 }
