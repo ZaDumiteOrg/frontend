70e346fd861327d23e1407e47dcae9cb6782c19b
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-02-03 02:02:29 +0200

    added user repository to repository module

--- app/src/main/java/com/example/zadumite_frontend/di/RepositoryModule.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/RepositoryModule.kt
@@ -1,10 +1,12 @@
 package com.example.zadumite_frontend.di
 
 import com.example.zadumite_frontend.data.repository.AuthRepository
+import com.example.zadumite_frontend.data.repository.UserRepository
 import com.example.zadumite_frontend.data.repository.WordRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single { AuthRepository(get(), get()) }
     single { WordRepository(get(), get()) }
+    single{ UserRepository(get(), get()) }
 }
