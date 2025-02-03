31a9ad7c1450477d4f4b175fffbd105f8a59ec7c
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-02-03 02:04:05 +0200

    added use cases to module

--- app/src/main/java/com/example/zadumite_frontend/di/UseCaseModule.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/UseCaseModule.kt
@@ -3,7 +3,9 @@ package com.example.zadumite_frontend.di
 import com.example.zadumite_frontend.domain.AddWordUseCase
 import com.example.zadumite_frontend.domain.FetchUserWordsUseCase
 import com.example.zadumite_frontend.domain.FetchWordUseCase
+import com.example.zadumite_frontend.domain.GetUserUseCase
 import com.example.zadumite_frontend.domain.LogInUseCase
+import com.example.zadumite_frontend.domain.LogoutUseCase
 import com.example.zadumite_frontend.domain.SignUpUseCase
 import org.koin.dsl.module
 
@@ -12,5 +14,7 @@ val useCaseModule = module {
     factory{ SignUpUseCase(get()) }
     factory{ LogInUseCase(get()) }
     factory{ FetchWordUseCase(get()) }
-    factory{ FetchUserWordsUseCase(get(), get())}
+    factory{ FetchUserWordsUseCase(get(), get()) }
+    factory{ GetUserUseCase(get(), get()) }
+    factory{ LogoutUseCase(get()) }
 }
