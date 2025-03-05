bcb0155d638bf303bc8b8b5acae0e695d0c30166
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-03-05 16:23:02 +0200

    base url set for emulator testing

--- app/src/main/java/com/example/zadumite_frontend/di/providers/provideRetrofit.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/providers/provideRetrofit.kt
@@ -4,7 +4,9 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+//private const val BASE_URL = "http://localhost:3000/"
 private const val BASE_URL = "http://10.0.2.2:3000/"
+
 fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
     return Retrofit.Builder()
         .baseUrl(BASE_URL)
