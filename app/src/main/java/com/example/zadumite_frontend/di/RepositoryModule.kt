04340adf9464e232b1a498fdb1e6a16563b7c850
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-05-23 13:56:26 +0300

    business logic

--- app/src/main/java/com/example/zadumite_frontend/di/RepositoryModule.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/RepositoryModule.kt
@@ -1,6 +1,7 @@
 package com.example.zadumite_frontend.di
 
 import com.example.zadumite_frontend.data.repository.AuthRepository
+import com.example.zadumite_frontend.data.repository.DailyQuestionRepository
 import com.example.zadumite_frontend.data.repository.UserRepository
 import com.example.zadumite_frontend.data.repository.WordRepository
 import org.koin.dsl.module
@@ -9,4 +10,5 @@ val repositoryModule = module {
     single { AuthRepository(get(), get()) }
     single { WordRepository(get(), get()) }
     single{ UserRepository(get(), get()) }
+    single{ DailyQuestionRepository(get()) }
 }
--- app/src/main/java/com/example/zadumite_frontend/di/providers/provideRetrofit.kt
+++ app/src/main/java/com/example/zadumite_frontend/di/providers/provideRetrofit.kt
@@ -4,8 +4,8 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-//private const val BASE_URL = "http://localhost:3000/"
-private const val BASE_URL = "http://10.0.2.2:3000/"
+//private const val BASE_URL = "http://192.168.0.11:3000/"
+private const val BASE_URL = "http://10.0.2.2:3000/" // for emulator testing
 
 fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
     return Retrofit.Builder()
