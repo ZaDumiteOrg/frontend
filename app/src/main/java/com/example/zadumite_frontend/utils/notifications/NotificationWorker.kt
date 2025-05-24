297681398d380e70b8a84e472c426189de4ad200
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-05-25 00:26:45 +0300

    updated notifications to show and removed hard coded strings

--- app/src/main/java/com/example/zadumite_frontend/utils/notifications/NotificationWorker.kt
+++ app/src/main/java/com/example/zadumite_frontend/utils/notifications/NotificationWorker.kt
@@ -1,32 +1,18 @@
 package com.example.zadumite_frontend.utils.notifications
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import android.util.Log
 import androidx.work.CoroutineWorker
-import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.example.zadumite_frontend.data.model.datastore.JwtTokenDataStore
-import com.example.zadumite_frontend.data.model.token.JwtTokenManager
-import java.util.prefs.Preferences
 
 class NotificationWorker(
     context: Context,
     workerParams: WorkerParameters,
-    private val jwtTokenManager: JwtTokenManager
 ) :  CoroutineWorker(context, workerParams) {
     override suspend fun doWork(): Result {
-        val context = applicationContext
-
-        val accessToken = jwtTokenManager.getAccessJwt()
-
-        if (accessToken.isNullOrEmpty()) {
-            return Result.success()
-        }
-
-        val notificationHandler = NotificationHandler(context)
+        Log.d("NotificationWorker", "Showing notification")
+        val notificationHandler = NotificationHandler(applicationContext)
         notificationHandler.showWordOfWeekNotification()
-
         return Result.success()
     }
 }
